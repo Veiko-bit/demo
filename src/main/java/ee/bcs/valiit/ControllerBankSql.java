bbe65b094edc05c29c40e37074963dc6b0dce48b
Veiko Ilves <veikoilves10@gmail.com>
2021-04-27 15:58:04 +0300

    git commit -m

--- src/main/java/ee/bcs/valiit/ControllerBankSql.java
+++ src/main/java/ee/bcs/valiit/ControllerBankSql.java
@@ -6,45 +6,45 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@RestController
 public class ControllerBankSql {
     @Autowired
     private ServiceBank serviceBank;
     @CrossOrigin
 
-    //http://localhost:8080/veiko/bank/createAccount1?accountNumber1=EE123§balance=100
-    @PostMapping("veiko/bank/createAccount1")
+    //http://localhost:8080/veiko/bank2/createAccount1?accountNumber1=EE123&balance=100
+    @PostMapping("veiko/bank2/createAccount1")
     public void createAccount1(@RequestParam("accountNumber1") String accountNr,
                                @RequestParam("balance") Double balance) {
         serviceBank.createAccount1 (accountNr, balance);
     }
 
-    //http://localhost:8080/veiko/bank/account/EE123
-    @GetMapping("veiko/bank/account/{accountNumber1}")
+    //http://localhost:8080/veiko/bank2/account/EE123
+    @GetMapping("veiko/bank2/account/{accountNumber1}")
     public String getBalance(@PathVariable("accountNumber1") String accountNr) {
         return serviceBank.getBalance(accountNr);
     }
 
-    //http://localhost:8080/veiko/bank/account/EE123/100
-    @PutMapping("veiko/bank/account/{accountNumber1}/{addDeposit}")
+    //http://localhost:8080/veiko/bank2/deposit/EE123/100
+    @PutMapping("veiko/bank2/deposit/{accountNumber1}/{addDeposit}")
     public Double addDeposit(@PathVariable("accountNumber1") String accountNr,
                              @PathVariable("addDeposit") Double amount) {
         return serviceBank.addDeposit(accountNr, amount);
     }
 
-    //http://localhost:8080/veiko/bank/account/EE123/30
-    @PutMapping("veiko/bank/account/{accountNumber}/{withdrawMoney}")
-    public String withdrawMoney(@PathVariable("accountNumber") String accountNr,
+    //http://localhost:8080/veiko/bank2/withdraw/EE123/30
+    @PutMapping("veiko/bank2/withdraw/{accountNumber}/{withdrawMoney}")
+    public Double withdrawMoney(@PathVariable("accountNumber") String accountNr,
                                 @PathVariable("withdrawMoney") Double amount) {
         return serviceBank.withdrawMoney(accountNr, amount);
     }
 
-    //http://localhost:8080/veiko/bank/account/EE123/EE124/20
-    @PutMapping("veiko/bank/account/{accountNumber1}/{accountNumber2}/{transferMoney}")
-    public String transferMoney(@PathVariable("accountNumber1") String fromAccountNr1,
+    //http://localhost:8080/veiko/bank2/account/EE123/EE124/20
+    @PutMapping("veiko/bank2/account/{accountNumber1}/{accountNumber2}/{transferMoney}")
+    public void transferMoney(@PathVariable("accountNumber1") String fromAccountNr1,
                                 @PathVariable("accountNumber2") String toAccountNr2,
                                 @PathVariable("transferMoney") Double amount) {
-        return serviceBank.transferMoney(fromAccountNr1, toAccountNr2, amount);
+        serviceBank.transferMoney(fromAccountNr1, toAccountNr2, amount);
     }
 }
 
